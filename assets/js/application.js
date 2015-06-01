8e404838f93827b331528d2ec0b26b201932d59d
pycal <cmcquattie@fourthwall.ca>
2015-06-01 09:59:12 -0400

    default dropdown values

--- assets/js/application.js
+++ assets/js/application.js
@@ -8,13 +8,37 @@ ammortizeApp.controller('AmortizationController', ['$scope', function ($scope) {
     $scope.principal = 120000;
     $scope.nominal_rate = 4.0;
     $scope.effective_rate = 4.0;
-    $scope.annual_compounding_periods = 2;
-    $scope.payments_per_year = 12;
     $scope.cmhc_required = false;
     $scope.mortgage_term = 25;
     $scope.total_cost_of_ammortization;
     $scope.total_cost_of_interest;
     $scope.data = [[],[],[]];
+    $scope.ppy_options = [
+        {
+          name: 'Monthly',
+          value: '12'
+        }, 
+        {
+          name: 'Bi-monthly',
+          value: '26'
+        },
+        {
+          name: 'Weekly',
+          value: '52'
+        }
+    ];
+    $scope.payments_per_year = $scope.ppy_options[0].value;
+    $scope.cp_options = [
+        {
+          name: 'Fixed rate',
+          value: '2'
+        }, 
+        {
+          name: 'Variable rate',
+          value: '12'
+        }
+    ];
+    $scope.annual_compounding_periods = $scope.cp_options[0].value;
 
     // Monthly costs
     $scope.condo_fees = 0;
