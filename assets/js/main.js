c3c90e308831f2538ea58fa8c96cd8f897ecd00c
pycal <cmcquattie@fourthwall.ca>
2015-07-03 11:40:36 -0400

    pop-up modal once only

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,12 +1,14 @@
 var has_focused = false;
+var has_seen_tip = localStorage.getItem("rta_tip");
 
 var open_tip_modal = function () {
   $('#tip_modal').modal();
+  localStorage.setItem("rta_tip", true);
 };
 
 
 $(".form-control").click( function () {
-	if (!has_focused) {
+	if (!has_focused && !rta_tip) {
 		open_tip_modal();
     has_focused = true;
 	}
